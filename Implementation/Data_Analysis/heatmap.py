9c2eb4821dd28068d38649d7aad27b5040502f93
Thore1997 <thore.johannsen@outlook.com>
2026-04-29 17:30:47 +0200

    small changes

--- Implementation/Data_Analysis/heatmap.py
+++ Implementation/Data_Analysis/heatmap.py
@@ -1,44 +1,21 @@
 import scipy.io
+import scipy.stats as stats  # Added this for Spearman
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+
 mat = scipy.io.loadmat('Reproduction/Data/wineori.mat')
 data = mat['X']
 
-corr_matrix = np.corrcoef(data, rowvar=False)
-mask = np.triu(np.ones_like(corr_matrix, dtype=bool))
-
+corr_matrix_spearman, _ = stats.spearmanr(data)
+mask_s = np.triu(np.ones_like(corr_matrix_spearman, dtype=bool))
 feature_name = [f"Feature {i+1}" for i in range(data.shape[1])]
 
 plt.figure(figsize=(12, 10))
 sns.heatmap(
-    corr_matrix,
-    mask=mask,
-    annot=True,
-    fmt=".6f",
-    cmap='coolwarm',
-    center=0,
-    annot_kws={"size": 9},
-    square=True,
-    linewidths=.5,           # Added comma here
-    xticklabels=feature_name, # Moved inside the parenthesis
-    yticklabels=feature_name  # Moved inside the parenthesis
-)                            # Moved the closing parenthesis to HERE
-
-plt.title("Correlation Heatmap")
-plt.tight_layout()
-plt.show()
-
-######## Spearman Correlation Heatmap #######
-mask = np.triu(np.ones_like(corr_matrix, dtype=bool))
-
-feature_name = [f"Feature {i+1}" for i in range(data.shape[1])]
-
-plt.figure(figsize=(12, 10))
-sns.heatmap(
-    corr_matrix,
-    mask=mask,
+    corr_matrix_spearman,
+    mask=mask_s,
     annot=True,
     fmt=".6f",
     cmap='coolwarm',
@@ -50,6 +27,5 @@ sns.heatmap(
     yticklabels=feature_name
 )
 
-plt.title("Spearman Rank Correlation Heatmap")
 plt.tight_layout()
 plt.show()
